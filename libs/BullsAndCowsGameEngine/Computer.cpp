9dfa29a92a3e2bbdfb74b7166cabd96fc7c10750
KostadinovK <kostadinovk2001@gmail.com>
2019-02-25 14:02:30 +0200

    Modified Number.cpp and .h and finished the Computer class

---
+++ libs/BullsAndCowsGameEngine/Computer.cpp
@@ -0,0 +1,50 @@
+#include <Computer.h>
+
+Computer::Computer()
+{
+    srand(time(NULL));
+    this->number = Number();
+}
+
+void Computer::generateNumber()
+{
+    int num = rand() % 9999 + 1000;
+    
+    this->number = Number(num);
+
+    while(this->hasDuplicates())
+    {
+        num = rand() % 9999 + 1000;
+        this->number = Number(num);
+    }
+}
+
+bool Computer::hasDuplicates()
+{
+    int value = this->number.getValue();
+    bool digs[10];
+
+    for(int i = 0;i < 9;i++)
+    {
+        digs[i] = false;
+    }
+
+    while(value > 0)
+    {
+        int digit = value % 10;
+
+        if(digs[digit])
+        {
+            return true;
+        }
+
+        digs[digit] = true;
+    }
+
+    return false;
+}
+
+Number Computer::getNumber()
+{
+    return this->number;
+}
