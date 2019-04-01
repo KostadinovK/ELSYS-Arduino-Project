0def093e3b5c20eeb5c246f2591d69f3a36cca3a
KostadinovK <kostadinovk2001@gmail.com>
2019-04-01 12:36:07 +0300

    Refactored global constants with #define

--- main/main.ino
+++ main/main.ino
@@ -3,10 +3,15 @@
 #include <Menu.h>
 #include <GameController.h>
 
-const byte ROWS = 4; 
-const byte COLS = 3;
-const int rs = 5, en = 4, d4 = 3, d5 = 2, d6 = A1, d7 = A0;
-
+#define ROWS 4
+#define COLS 4
+#define RS 5
+#define EN 4
+#define D4 3
+#define D5 2
+#define D6 A1
+#define D7 A0
+ 
 char hexaKeys[ROWS][COLS] = {
   {'1', '2', '3'},
   {'4', '5', '6'},
@@ -17,7 +22,7 @@ char hexaKeys[ROWS][COLS] = {
 byte rowPins[ROWS] = {13, 12, 11, 10}; 
 byte colPins[COLS] = {9, 8, 7};
 
-LiquidCrystal screen(rs, en, d4, d5, d6, d7);
+LiquidCrystal screen(RS, EN, D4, D5, D6, D7);
 Keypad keyPad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 String options[] = {"1.Bulls and Cows", "2.Dinosaur Game"};
